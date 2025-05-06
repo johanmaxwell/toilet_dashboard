72b9eef2ff74933cd18e32a16dd7788ed2ca29a5
Johan Maxwell Christian <c11210020@john.peter.ac.id>
2025-05-06 21:02:19 +0700

    controller added

---
+++ app/Http/Controllers/TopupController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Midtrans\Config;
+
+class TopupController extends Controller
+{
+    public function process(Request $request)
+    {
+        \Midtrans\Config::$serverKey = config('midtrans.server_key');
+        \Midtrans\Config::$isProduction = false;
+        \Midtrans\Config::$isSanitized = true;
+        \Midtrans\Config::$is3ds = true;
+
+        $firstName = $request->input('first_name');
+        $lastName = $request->input('last_name');
+        $email = $request->input('email');
+        $phone = $request->input('phone');
+        $plan = $request->input('plan');
+
+        $plans = [
+            'small' => ['amount' => 15000, 'credit' => 200000],
+            'normal' => ['amount' => 60000, 'credit' => 1000000],
+            'big' => ['amount' => 250000, 'credit' => 5000000],
+        ];
+
+        if (!isset($plans[$plan])) {
+            return response()->json(['error' => 'Invalid plan selected.'], 400);
+        }
+
+        $orderId = uniqid('TOPUP-');
+
+        $params = array(
+            'transaction_details' => array(
+                'order_id' => $orderId,
+                'gross_amount' => $plans[$plan]['amount'],
+            ),
+            'customer_details' => array(
+                'first_name' => $firstName,
+                'last_name' => $lastName,
+                'email' => $email,
+                'phone' => $phone,
+            ),
+        );
+
+        $snapToken = \Midtrans\Snap::getSnapToken($params);
+        $redirectUrl = "https://app.sandbox.midtrans.com/snap/v2/vtweb/" . $snapToken;
+
+        //return redirect($redirectUrl);
+        return response()->json(['snap_token' => $snapToken]);
+    }
+
+    public function callback(Request $request) {}
+}
